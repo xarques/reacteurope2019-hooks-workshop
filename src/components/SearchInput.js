74257715f20b4e4998f28b984ece39f82d66f21e
Xavier Arques <xavarques@gmail.com>
2019-05-22 10:05:14 +0200

    Autofocus version 1

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Input } from '@smooth-ui/core-sc';
 
 import { useT } from './I18n';
 
-export default props => (
-  <Input
-    placeholder={useT('placeholder')}
-    width="100%"
-    type="search"
-    {...props}
-  />
-);
+export default props => {
+  const inputRef = useRef();
+
+  useEffect(() => inputRef.current.focus(), []);
+
+  return (
+    <Input
+      ref={inputRef}
+      placeholder={useT('placeholder')}
+      width="100%"
+      type="search"
+      {...props}
+    />
+  );
+};
